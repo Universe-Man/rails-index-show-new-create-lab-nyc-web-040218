03cc7f5868b0db255c8535e9173d36b9f3c4b517
github username <ianjpollack@gmail.com>
2018-04-30 21:09:49 -0400

    2 tests left

---
+++ app/controllers/coupons_controller.rb
@@ -0,0 +1,21 @@
+class CouponsController < ApplicationController
+
+  def index
+    @coupons = Coupon.all
+  end
+
+  def show
+    @coupon = Coupon.find(params[:id])
+  end
+
+  def new
+  end
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  def create
+    @coupon = Coupon.create(coupon_code: params[:coupon_code], store: params[:store])
+    binding.pry
+    @coupon.save
+    redirect_to @coupon
+  end
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+end
